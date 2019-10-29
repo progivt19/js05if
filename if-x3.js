d708d9e42e11456cc8ba985a15c3973abe10c813
Prokop1y <54879791+pstn72@users.noreply.github.com>
2019-10-29 11:28:37 +0900

    11zad

--- if-x3.js
+++ if-x3.js
@@ -1,10 +1,28 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
+//11 zadanie
 
-function ...(){
-
+function WeekdayName(n){
+	switch (n){
+		case 1:
+		Weekday Name = 'Понедельник'; break;
+		case 2:
+		Weekday Name = 'Вторник'; break;
+		case 3:
+		Weekday Name = 'Среда'; break;
+		case 4:
+		Weekday Name = 'Четверг'; break;
+		case 5:
+		Weekday Name = 'Пятница'; break;
+		case 6:
+		Weekday Name = 'Суббота'; break;
+		case 7:
+		Weekday Name = 'Воскресенье'; break;
+		default: WeekdayName = '';
+	}
+return WeekdayName;
 
 }
 
-module.exports = ...;
+module.exports = WeekdayName;
