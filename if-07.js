8e37f5b8734861ad3e6a5f73600929ab70303ddb
Вешников Валерий <Valeraiq@mail.ru>
2019-10-29 21:27:36 +0900

    Доработано

--- if-07.js
+++ if-07.js
@@ -14,6 +14,12 @@ function S(x1, y1, x2, y2, x3, y3){
 	var ac = Math.sqrt(((x3 - x1) * (x3 - x1)) + ((y3 - y1) * (y3 - y1)));
 	var bc = Math.sqrt(((x3 - x2) * (x3 - x2)) + ((y3 - y2) * (y3 - y2)));
 
+	if(x1 == x2 && x1 == x3 && x2 == x3){
+		return d;
+	}
+	if(y1 == y2 && y1 == y3 && y2 == y3){
+		return d;
+	}
 	if(ab == ac){
 		d = ((bc*(Math.sqrt((ab*ab)-((bc/2)*(bc/2)))))/2);
 	}
