4c1f5ff2c437d3f7643d46b2137a32dce54f8b35
ganyavasiliev <54825242+ganyavasiliev@users.noreply.github.com>
2019-11-01 15:37:14 +0900

    Update if-x1.js

--- if-x1.js
+++ if-x1.js
@@ -2,9 +2,26 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
+function f(a, b, c, d){
+	var ab=a+b;
+	var cd=c+d;
 
-
+	if(ab>cd || ab==cd){
+		if(c>d || c==d){
+			return c
+		}
+		if(c<d){
+			return d
+		}
+	};
+	if(ab<cd || ab==cd){
+		if(a>b || a==b){
+			return a
+		}
+		if(a<b){
+			return b;
+		}
+	}
 }
 
-module.exports = ...;
+module.exports = f;
