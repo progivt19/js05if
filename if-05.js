b0c0a6d14660a3a1393ff5d34508d0fddc593695
jixonka <49236474+jixonka@users.noreply.github.com>
2019-11-01 23:20:59 +0900

    Tests1AbramovSE

---
+++ if-05.js
@@ -0,0 +1,12 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function h(a, b, c){
+	if(a+b > c && a+c > b && b+c > a){
+		p = (a+b+c)/2
+		return 2*Math.sqrt(p*(p-a)*(p-b)*(p-c))/a
+	}
+}
+
+module.exports = h;
---
+++ if-10.js
@@ -0,0 +1,47 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function MonthName(n){
+	switch(n){
+		case 1:
+			return('Январь')
+			break;
+		case 2:
+			return('Февраль')
+			break;
+		case 3:
+			return('Март')
+			break;
+		case 4:
+			return('Апрель')
+			break;
+		case 5:
+			return('Май')
+			break;
+		case 6:
+			return('Июнь')
+			break;
+		case 7:
+			return('Июль')
+			break;
+		case 8:
+			return('Август')
+			break;
+		case 9:
+			return('Сентябрь')
+			break;
+		case 10:
+			return('Октябрь')
+			break;
+		case 11:
+			return('Ноябрь')
+			break;
+		case 12:
+			return('Декабрь')
+			break;
+		default:
+			return('Такого месяца нет')
+	}
+}
+module.exports = MonthName;
