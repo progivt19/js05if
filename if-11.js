48703d557f92cdce8a3eaa033404e15c936c061c
Ustinov-Ki <54973177+Ustinov-Ki@users.noreply.github.com>
2019-11-05 15:06:00 +0900

    болванка

---
+++ if-11.js
@@ -0,0 +1,27 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function WeekDayName(n){
+    switch (n) {
+        case (1):
+            return('Понедельник');
+        case (2):
+            return('Вторник');
+        case (3):
+            return('Среда');
+        case (4):
+            return('Четверг');
+        case (5):
+            return('Пятница');
+        case (6):
+            return('Суббота');
+        case (7):
+            return('Воскресенье');
+        default:
+            return ('нет такого дня');
+    }
+
+}
+
+module.exports = WeekDayName;
