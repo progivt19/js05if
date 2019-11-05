5ab3a17676432f2035d02aa24012aadccd561683
keltoblast <55118185+keltoblast@users.noreply.github.com>
2019-11-05 11:06:40 +0900

    работа 5

--- if-x3.js
+++ if-x3.js
@@ -1,10 +1,17 @@
-//TODO: Впишите имя функции из задания вместо ...
+//TODO: Николаев Сандал
 //TODO: Добавьте правильные параметры
-//TODO: Напишите функцию
+//TODO: h(a, b, c)
 
-function ...(){
-
+function h(a, b, c){
+	if (a+b>c && a+c>b && b+c>a) {
+		var p = (a+b+c) / 2;
+		var s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		var h = (2*s) / a;
+		return h;
+	}else{
+		var h = -1;
+		return h;
+	}
 
 }
-
-module.exports = ...;
+module.exports = h;
