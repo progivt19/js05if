74d45e8a5b82ce469f276844bd3ac25c3401f3da
ganyavasiliev <54825242+ganyavasiliev@users.noreply.github.com>
2019-11-05 09:49:53 +0900

    Sdelal

--- if-06.js
+++ if-06.js
@@ -2,9 +2,17 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
-
-
+function f(k, b, R){
+	var d = k * k * R * R - b * b + R * R;
+	if (d > 0){
+		return 2;
+	}
+	else if (d === 0){
+		return 1;
+	}
+	else if (d < 0){
+		return 0;
+	}
 }
 
-module.exports = ...;
+module.exports = f;
