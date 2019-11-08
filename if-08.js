dda53fc806d9f98a93f2ee6752a6247ca2a75cb1
grigorevion <54822615+grigorevion@users.noreply.github.com>
2019-11-08 15:29:39 +0900

    Update if-08.js

--- if-08.js
+++ if-08.js
@@ -5,7 +5,7 @@
 function S(x1, y1, x2, y2, x3, y3){
 	var opr = ((x1 - x3) * (y2 - y3)) - ((x2 - x3) * (y2 - y3));
 	var ab = Math.sqrt( ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) );
-	var ac = Math.sqrt( ((x1 - x3) * (x1 - x3)) + ((y1 - y3) * (y1 - y3)) );
+	var ac = Math.sqrt( ((x3 - x1) * (x3 - x1)) + ((y3 - y1) * (y3 - y1)) );
 	var bc = Math.sqrt( ((x3 - x2) * (x3 - x2)) + ((y3 - y2) * (y3 - y2)) );
 	if( (ab + ac > bc || ab + bc > ac || ac + bc > ab) && ((ab * ab) + (ac * ac) > (bc * bc) || (ab * ab) + (bc * bc) > (ac & ac) || (ac *ac) + (bc * bc) > (ab * ab)) ) {
 		return opr * 0.5;
