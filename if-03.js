2d8aba80b1571e0641f105d54c078cfa100ce4d6
7nya <54824848+7nya@users.noreply.github.com>
2019-11-08 15:31:16 +0900

    Сделал  3

---
+++ if-03.js
@@ -0,0 +1,25 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+var max1 = 0;
+var max2 = 0;
+
+function f(a,b,c,d){
+    if (a >= b){
+        max1 = a
+    }else{
+        max1 = b
+}
+    if (c >= d){
+        max2 = c
+	}else{
+        max2 = d
+}
+    if (max1 <= max2){
+        return (max1)
+    }else{
+        return (max2)
+    }
+}
+
+module.exports = f;
