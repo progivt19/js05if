8c22a23f13c5098fa60bc6226b0b47a9ae768fce
7nya <54824848+7nya@users.noreply.github.com>
2019-11-08 15:31:35 +0900

    Сделал 7

---
+++ if-07.js
@@ -0,0 +1,20 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function s(x1,y1,x2,y2,x3,y3){
+	var a = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
+	var b = Math.sqrt(Math.pow(x2-x3,2) + Math.pow(y2-y3,2));
+	var c = Math.sqrt(Math.pow(x3-x1,2) + Math.pow(y3-y1,2));
+
+if ((a == b || b == c || c == a) && (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)) {
+
+	var p = (a+b+c)/2;
+
+	return Math.floor(Math.sqrt (p*(p-a)*(p-b)*(p-c)));
+}else{ 
+	return -1;
+  }
+}
+
+module.exports = s;
