58db652e34289d456c6c7905fdff18e3fd4fafcc
grigorevion <54822615+grigorevion@users.noreply.github.com>
2019-11-08 15:27:08 +0900

    Update and rename if-04_test.js to if-12_test.js

---
+++ test/if-12_test.js
@@ -0,0 +1,11 @@
+const digitName = require('../if-12.js');
+
+QUnit.test("ЗАДАЧА 12}", 
+  function(assert){
+	assert.deepEqual(digitName(1), "ONE", "Не выполнено n >= 0 && n <= 9 ");
+	assert.deepEqual(digitName(3), "THREE", "Не выполнено n >= 0 && n <= 9");
+	assert.deepEqual(digitName(5), "FIVE", "Не выполнено n >= 0 && n <= 9");
+	assert.deepEqual(digitName(-1), "ERROR", "Не выполнено n <= 0");
+	assert.deepEqual(digitName(10), "ERROR", "Не выполнено n >= 9");
+  }
+);
