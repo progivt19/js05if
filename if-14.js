3b2495a2b526574f3c1b657a011d63a859c21638
ganyavasiliev <54825242+ganyavasiliev@users.noreply.github.com>
2019-11-08 14:08:28 +0900

    VIPOLNIL

--- if-14.js
+++ if-14.js
@@ -2,9 +2,43 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
-
-
+function X4(x1, y1, x2, y2, x3, y3){
+	if(x1 == x2 && x1 == x3 && x2 == x3){
+		return "Ошибка";
+	}
+	if(x1 == x2){
+		return x3;
+	}
+	if(x1 == x3){
+		return x2;
+	}
+	if(x2 == x3){
+		return x1;
+	}
+	else{
+		return (x1+x3-x2);
+	}
 }
 
-module.exports = ...;
+function Y4(x1, y1, x2, y2, x3, y3){
+	if(y1 == y2 && y1 == y3 && y2 == y3){
+		return "Ошибка";
+	}
+	if(X4(x1, y1, x2, y2, x3, y3) == "Ошибка"){
+		return "Ошибка";
+	}
+	if(y2 == y3){
+		return y1;
+	}
+	if(y1 == y2){
+		return y3;
+	}
+	if(y1 == y3){
+		return y2;
+	}
+	else{
+		return (y1+y3-y2);
+	}
+}
+
+module.exports = [X4, Y4];
