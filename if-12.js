cc25c9cd6ba79b71bb41514a9113994ef68d3ef5
7nya <54824848+7nya@users.noreply.github.com>
2019-11-08 15:32:04 +0900

    Сделал 12

---
+++ if-12.js
@@ -0,0 +1,42 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+function f(n) {
+   switch(n)
+      {
+      	case 0 :
+            f = ("zero"); 
+            break;
+         case 1 :
+            f = ("one"); 
+            break;
+         case 2 :
+            f = ("two"); 
+            break;
+         case 3 :
+            f = ("three");
+            break;
+         case 4 :
+            f = ("four");
+            break;
+         case 5 :
+            f = ("five");
+            break;
+         case 6 :
+            f = ("six"); 
+            break;
+         case 7 :
+            f = ("seven"); 
+            break;
+         case 8 :
+            f = ("eight"); 
+            break;
+         case 9 :
+            f = ("nine"); 
+            break;
+         default :
+            f = ("Error");
+}
+return f;
+}
+module.exports =  f;
