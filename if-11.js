a8afd89a8395947773507466abb17c560931b93b
Adamov <aiaal.adamov@gmail.com>
2019-11-10 23:23:28 +0900

    Sdelal i ispravil

---
+++ if-11.js
@@ -0,0 +1,18 @@
+s//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function WeekdayName(n){
+	switch (n){
+		case 1: return "понедельник"; break;
+		case 2: return "вторник"; break;
+		case 3: return "среда"; break;
+		case 4: return "четверг"; break;
+		case 5: return "пятница"; break;
+		case 6: return "суббота"; break;
+		case 7: return "воскресенье"; break;
+		default: "";
+	}
+}
+
+module.exports = WeekdayName;
---
+++ if-13.js
@@ -0,0 +1,30 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function S(x1,y1,x2,y2,x3,y3){
+	if (y1===y2 && ((x1===x3)||(x2===x3)) && Math.abs(x2-x1)===(Math.abs(y2-y3)||(Math.abs(y1-y3)))){
+		return "true";
+	}
+	if (y1===y3 && ((x1===x2)||(x3===x2)) && Math.abs(x3-x1)===(Math.abs(y3-y2)||(Math.abs(y1-y2)))){
+		return "true";
+	}
+	if (y2===y1 && ((x2===x3)||(x1===x3)) && Math.abs(x2-x1)===(Math.abs(y2-y3)||(Math.abs(y1-y3)))){
+		return "true";
+	}
+	if (y2===y3 && ((x2===x1)||(x3===x1)) && Math.abs(x2-x3)===(Math.abs(y2-y1)||(Math.abs(y3-y1)))){
+		return "true";
+	}
+	if (y3===y1 && ((x3===x2)||(x1===x2)) && Math.abs(x3-x1)===(Math.abs(y3-y2)||(Math.abs(y1-y2)))){
+		return "true";
+	}
+	if (y3===y2 && ((x3===x1)||(x2===x1)) && Math.abs(x3-x2)===(Math.abs(y3-y1)||(Math.abs(y2-y1)))){
+		return "true";
+	if Math.sqrt(Math.pow(Math.abs(x1-x2))+Math.pow(Math.abs(y1-y2))
+	}
+	else{
+		return "false";
+	}
+}
+
+module.exports = S;
