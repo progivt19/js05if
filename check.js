8f216c4296260f9403963bc372cabdb7a64768db
Авксентий Бузинаев <7Nya95@mail.ru>
2019-11-10 11:15:17 +0900

    сделал 14

--- check.js
+++ check.js
@@ -1,3 +1,3 @@
 var s = require('./if-07.js');
 
-console.log(s(-2,0,2,1,1,6));
+console.log(s(0,3,3,0,0,0));
---
+++ if-14.js
@@ -0,0 +1,47 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function X4(x1, y1, x2, y2, x3, y3){
+	if(x1 == x2 && x1 == x3 && x2 == x3){
+
+		return "ошибка";
+	}
+	if(x1 == x2) {
+		return x3;
+	}
+	if(x1 == x3) {
+		return x2;
+	}
+	if(x2 == x3) {
+		return x1;
+	}
+	else{
+		return (x1+x3-x2);
+	}
+}
+
+function Y4(x1, y1, x2, y2, x3, y3) {
+	if(y1 == y2 && y1 == y3 && y2 == y3) {
+
+		return "ошибка";
+	}
+	if(X4(x1, y1, x2, y2, x3, y3) == "ошибка") {
+
+		return "ошибка";
+	}
+	if(y2 == y3) {
+		return y1;
+	}
+	if(y1 == y2) {
+		return y3;
+	}
+	if(y1 == y3) {
+		return y2;
+	}
+	else{
+		return (y1 + y3 - y2);
+	}
+}
+
+module.exports = [X4, Y4];
