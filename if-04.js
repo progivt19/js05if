4093d62af344f547012f40b0bccc57e214fd0288
Vasilyeva <bdcapcap@gmail.com>
2019-11-11 14:39:34 +0900

    4

--- if-04.js
+++ if-04.js
@@ -2,9 +2,28 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
+function f(a, b, c, d){
+	var min, max
+	a1 = Math.abs(a), 
+	b1 = Math.abs(b),
+	c1 = Math.abs(c),
+	d1 = Math.abs(d);
+	if (a1 <= b1)
+		min = a1;
+	else
+		min = b1;
+	
+	if (c1 <= d1)
+		max = d1;
+	else
+		max = c1;
 
+	if (max >= min)
+		return max;
+	else
+		return min;
 
 }
 
-module.exports = ...;
+
+module.exports = f;
