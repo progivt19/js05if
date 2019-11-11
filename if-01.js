694fd67167663366e1ba0bf38cb6c09c589f16b8
AlekseevaDai <izumo-kun@mail.ru>
2019-11-11 15:14:32 +0900

    hello world

--- if-01.js
+++ if-01.js
@@ -1,10 +1,15 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
-
-function ...(){
-
-
+function f(x, y, z){
+	if (x+y+z >= x*y*z && x+y+z >= x*y+z){
+		return x + y + z;;
+	}
+	if (x*y*z >= x+y+z && x*y*z >= x*y+z ){;
+		return x*y*z;
+	}
+	if (x*y+z >= x+y+z && x*y+z >= x*y*z){
+		return x*y+z;
+	}
 }
-
-module.exports = ...;
+module.exports = f;
