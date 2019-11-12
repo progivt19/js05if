3b3b5a954f58ca5b2153da1171541cd90256c418
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2019-11-12 11:18:39 +0900

    13 zad

--- if-x4.js
+++ if-x4.js
@@ -2,32 +2,28 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function S(x1,y1,x2,y2,x3,y3)
-{
-	var a = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-	var b = Math.sqrt((x3-x2)^2 + (y3-y2)^2);
-	var c = Math.sqrt((x3-x1)^2 + (y3-y1)^2);
-	var d = 0;
-	if (a == b) {
-		d = Math.sqrt(a*a+b*b);
-		if (d = c*Math.sqrt(2)) {
-			return true;
-		}
+function S(x1,y1,x2,y2,x3,y3){
+	if (y1===y2 && ((x1===x3)||(x2===x3)) && Math.abs(x2-x1)===(Math.abs(y2-y3)||(Math.abs(y1-y3)))){
+		return "true";
 	}
-	else if (a == c) {
-		d = Math.sqrt(a*a+c*c);
-		if (d = b*Math.sqrt(2)) {
-			return true;
-		}
+	if (y1===y3 && ((x1===x2)||(x3===x2)) && Math.abs(x3-x1)===(Math.abs(y3-y2)||(Math.abs(y1-y2)))){
+		return "true";
 	}
-	else if (b == c) {
-		d = Math.sqrt(b*b+c*c);
-		if (d = a*Math.sqrt(2)) {
-			return true;
-		}	
+	if (y2===y1 && ((x2===x3)||(x1===x3)) && Math.abs(x2-x1)===(Math.abs(y2-y3)||(Math.abs(y1-y3)))){
+		return "true";
 	}
-	return false;
+	if (y2===y3 && ((x2===x1)||(x3===x1)) && Math.abs(x2-x3)===(Math.abs(y2-y1)||(Math.abs(y3-y1)))){
+		return "true";
+	}
+	if (y3===y1 && ((x3===x2)||(x1===x2)) && Math.abs(x3-x1)===(Math.abs(y3-y2)||(Math.abs(y1-y2)))){
+		return "true";
+	}
+	if (y3===y2 && ((x3===x1)||(x2===x1)) && Math.abs(x3-x2)===(Math.abs(y3-y1)||(Math.abs(y2-y1)))){
+		return "true";
+	}
+	else{
+		return "false";
+	}
 }
-
 
 module.exports = S;
