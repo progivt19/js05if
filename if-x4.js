5625634958ac9ec642b299760f4f0a3164033d9a
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2019-11-12 11:10:04 +0900

    13 zad

--- if-x4.js
+++ if-x4.js
@@ -2,9 +2,32 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
-
-
+function S(x1,y1,x2,y2,x3,y3)
+{
+	var a = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+	var b = Math.sqrt((x3-x2)^2 + (y3-y2)^2);
+	var c = Math.sqrt((x3-x1)^2 + (y3-y1)^2);
+	var d = 0;
+	if (a == b) {
+		d = Math.sqrt(a*a+b*b);
+		if (d = c*Math.sqrt(2)) {
+			return true;
+		}
+	}
+	else if (a == c) {
+		d = Math.sqrt(a*a+c*c);
+		if (d = b*Math.sqrt(2)) {
+			return true;
+		}
+	}
+	else if (b == c) {
+		d = Math.sqrt(b*b+c*c);
+		if (d = a*Math.sqrt(2)) {
+			return true;
+		}	
+	}
+	return false;
 }
 
-module.exports = ...;
+
+module.exports = S;
