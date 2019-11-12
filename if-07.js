051049828a33fc280d8fd3ca6ae0a6b64277276b
Ayal Okhlopkov <uhaato@gmail.com>
2019-11-12 10:33:34 +0900

    Исправление ошибок

--- if-07.js
+++ if-07.js
@@ -4,12 +4,9 @@ function S(x1, y1, x2, y2, x3, y3){
     var sides = [];
     var count = 0;
     for (var i = 0; i < 3; i++){
-        if(i != 2)
-            sides.push(distance(coordinateX[i], coordinateY[i], coordinateX[i+1], coordinateY[i+1]));
-        else
-            sides.push(distance(coordinateX[i], coordinateY[i], coordinateX[0], coordinateY[0]));
+        sides.push(distance(coordinateX[i], coordinateY[i], coordinateX[(i+1)%3], coordinateY[(i+1)%3]));
     }
-    if(sides[0] == sides[1] | sides[1] == sides [2] | sides[0] == sides[2])
+    if((sides[0] == sides[1] || sides[1] == sides [2] || sides[0] == sides[2]) && sides[0] != 0 && sides[1] != 0 && sides[2] != 0)
         return Math.abs(delta(x1, y1, x2, y2, x3, y3)) / 2;
     else
         return -1;
--- if-14.js
+++ if-14.js
@@ -1,11 +1,28 @@
 var vectorA = [0, 0];
 var vectorB = [0, 0];
+var ic = true;
 
 function X4(x1, y1, x2, y2, x3, y3){
-    return -vectorA[0]+x3;
+    if((x2-x1)*(x3-x1)+(y2-y1)*(y3-y1) == 0){
+        return x1 + (x2-x1) + (x3-x1);
+    }else if((x1-x2)*(x3-x2)+(y1-y2)*(y3-y2) == 0){
+        return x2 + (x1-x2) + (x3-x2);
+    }else if((x1-x3)*(x2-x3)+(y1-y3)*(y2-y3) == 0){
+        return x3 + (x1-x3) + (x2-x3);
+    }else{
+        ic = false;
+    }
 }
 function Y4(x1, y1, x2, y2, x3, y3){
-    return -vectorA[1]+y3;
+    if((x2-x1)*(x3-x1)+(y2-y1)*(y3-y1) == 0){
+        return y1 + (y2-y1) + (y3-y1);
+    }else if((x1-x2)*(x3-x2)+(y1-y2)*(y3-y2) == 0){
+        return y2 + (y1-y2) + (y3-y2);
+    }else if((x1-x3)*(x2-x3)+(y1-y3)*(y2-y3) == 0){
+        return x3 + (y1-y3) + (y2-y3);
+    }else{
+        ic = false;
+    }
 }
 
 function f(x1, y1, x2, y2, x3, y3){
@@ -13,14 +30,11 @@ function f(x1, y1, x2, y2, x3, y3){
     vectorA[1] = y2 - y1;
     vectorB[0] = x3 - x2;
     vectorB[1] = y3 - y2;
-    if(cos() == 0){
+    if(ic == true){
         return 'X = '+X4(x1, y1, x2, y2, x3, y3)+', Y = '+Y4(x1, y1, x2, y2, x3, y3);
     }else{
         return 'Данные точки не могут быть вершинами прямоугольника';
     }
-}
-function cos(){
-    return ((vectorA[0]*vectorB[0]) + (vectorA[1]*vectorB[1]))/(Math.sqrt(Math.pow(vectorA[0], 2) + Math.pow(vectorA[1], 2))*Math.sqrt(Math.pow(vectorB[0], 2) + Math.pow(vectorB[1], 2)));
 }
 
 module.exports = f;
