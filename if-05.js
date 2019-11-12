47aec75ce6ec454cff6c65afb469cb4d4aca5794
o-hash <54824932+o-hash@users.noreply.github.com>
2019-11-12 10:54:26 +0900

    что сделано

--- if-05.js
+++ if-05.js
@@ -3,7 +3,6 @@
 //TODO: Напишите функцию
 
 function h(a, b, c){
-  
      if (a + b > c && a + c && b + c > a) {
          var p = (a + b + c) / 2;
          return (2 / a) * (Math.sqrt(p * (p - a) * (p - b) * (p - c)))
