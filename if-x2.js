5390683ef916497ae42a4f9c0a47fee9b923f5b8
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2019-11-12 11:09:28 +0900

    6 zad

--- if-x2.js
+++ if-x2.js
@@ -1,10 +1,13 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
-
-function ...(){
-
-
+//6 zadanie
+function f(k, b, R){
+	var D;
+	D = (4*k*k*b*b)-4*(k*k+1)*(b*b-R*R);
+	if (D === 0) return 1;
+	if (D > 0) return 2;
+	if (D < 0) return 0;
 }
 
-module.exports = ...;
+module.exports = f;
