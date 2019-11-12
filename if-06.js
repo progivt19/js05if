0b89ace8452a1d1f2da108afd6e66b6a4783e427
Sivfrye <liza.sivseva@gmail.com>
2019-11-12 10:31:59 +0900

    лаба№5

--- if-06.js
+++ if-06.js
@@ -2,9 +2,15 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
+function fmax(a,b,c,d){
+var min1=0; 
+var min2=0;
 
-
+if (a<b) {min1=a} else {min1=b};
+if (c<d) {min2=c} else	{min2=d};
+if (min1<min2) 
+	return min1
+ else min2;
 }
 
-module.exports = ...;
+module.exports = fmax;
