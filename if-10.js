caebc44463583a3c8e0d9602a5a92a9177792e19
o-hash <54824932+o-hash@users.noreply.github.com>
2019-11-12 11:07:10 +0900

    что сделано

--- if-10.js
+++ if-10.js
@@ -3,46 +3,46 @@
 //TODO: Напишите функцию
 
 function MonthName(n){
-	switch(n) {
-		case 1:
-		    return "Январь";
-		    break;
-		case 2:
-		    return "Февраль";
-		    break;
-		case 3:
-		    return "Март";
-		    break;
-		case 4:
-		    return "Апрель";
-		    break;
-		case 5:
-		    return "Май";
-		    break;
-		case 6:
-            return "Июнь";
-            break;
-        case 7:
-            return "Июль";
-            break;
-        case 8:
-            return "Август";
-            break;
-        case 9:
-            return "Сентябрь"; 
-            break;
-        case 10:
-            return "Октябрь";
-            break;
-        case 11:
-            return "Ноябрь";
-            break;
-        case 12:
-            return "Декабрь";
-            break;
-        default:
-            return "error";
-            break;
+switch(n) {
+case 1:
+        return "Январь";
+        break;
+case 2:
+	return "Февраль";
+	break;
+case 3:
+	return "Март";
+	break;
+case 4:
+	return "Апрель";
+	break;
+case 5:
+	return "Май"; 
+	break;
+case 6:
+        return "Июнь";
+        break;
+case 7:
+        return "Июль";
+        break;
+case 8:
+        return "Август";
+        break;
+case 9:
+        return "Сентябрь"; 
+        break;
+case 10:
+        return "Октябрь";
+        break;
+case 11:
+        return "Ноябрь";
+        break;
+case 12:
+        return "Декабрь";
+        break;
+default:
+        return "error";
+        break;
 	}
 }
 
