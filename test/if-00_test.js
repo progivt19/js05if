7b8d2bef0fbb3e1d22d3898dc7f47fed5ed6bb94
Dusha628 <dushaborisov766@yandex.ru>
2019-11-12 14:23:49 +0900

    изменил

--- test/if-00_test.js
+++ test/if-00_test.js
@@ -1,10 +1,13 @@
 const f = require('../if-03.js');
 
 QUnit.test("ЗАДАЧА 03. f(a,b,c,d)=min(max(a,b), max(c,d)", function(assert){
-	assert.deepEqual(f( 1, 2, 3, 4), "Не выполнено f( 1, 1, 1) === min {3,1,2} === 1")
-	assert.deepEqual(f(-1, 0,-1),-2, "Не выполнено f(-1, 0,-1) === min {-2,0,-1} === -2")
-	assert.deepEqual(f(-1,-2, 1),-2, "Не выполнено f(-1,-2, 1) === min {-2,2,3} === -2")
-	assert.deepEqual(f( 0, 0, 0), 0, "Не выполнено f( 0, 0, 0) === min {0,0,0} === 0")
-	assert.deepEqual(f(-1, 1, 0),-1, "Не выполнено f(-1, 1, 0) === min {0,0,-1} === -1")
-	assert.deepEqual(f(-2, 0, 2), 0, "Не выполнено f(-2, 0, 2) === min {0,0,2} === 0")
+	assert.deepEqual(f( 1, 2, 3, 4), 2, "Не выполнено")
+	assert.deepEqual(f(-1, -2, -3, -4), -3, "Не выполнено")
+	assert.deepEqual(f(8, 6, 4, 2), 4, "Не выполнено")
+	assert.deepEqual(f( 2, 8, 4, 6), 6, "Не выполнено")
+	assert.deepEqual(f(2, 6, 8, 4), 6, "Не выполнено")
+	assert.deepEqual(f(2, 2, 2, 2), 2, "Не выполнено")
+	assert.deepEqual(f(2, 2, 4, 4), 2, "Не выполнено")
+	assert.deepEqual(f(-2, -2, -2, -2), -2, "Не выполнено")
+	assert.deepEqual(f(-2, -2, -4, -4), -4, "Не выполнено")
 });
