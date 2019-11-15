e6be0c330018fc2194e81e2c90875a8a492c4fea
Вешников Валерий <Valeraiq@mail.ru>
2019-11-15 15:31:41 +0900

    Исправил, теперь точно должно работать!

--- if-14.js
+++ if-14.js
@@ -11,23 +11,44 @@ function X4(x1, y1, x2, y2, x3, y3){
 	if(Math.abs(x1) == Math.abs(y1) && Math.abs(x2) == Math.abs(y2) && Math.abs(x3) == Math.abs(y3)){
 		return "Ошибка";
 	}
+	if(x1 == x3 && x2 > x1 && y1 == y2){
+		return (x1 + x2);
+	}
+	if(x1 == x3 && x2 < x1 && y1 == y2){
+		return (x1 - x2);
+	}
+	if(x1 == x3 && x2 > x1){
+		return (x1 - x2);
+	}
+	if(x1 == x3 && x2 < x1){
+		return (x1 + x2);
+	}
 	if(x1 == x2){
 		return x3;
-	}
-	if(x1 == x3){
-		return x2;
 	}
 	if(x2 == x3){
 		return x1;
 	}
-	if(x3 < x2){
+	if(x3 < x2 + x1){
 		return (x1 + x3 - x2);
 	}
-	if(x3 > x2){
+	if(x3 > x2 + x1){
 		return (x3 - x1 + x2);
 	}
+	if(x2 < x1 && x3 > x1){
+		return (x1 - x2 + x3);
+	}
+	if(x2 < x1 && x3 < x1){
+		return (x3 - x2 + x1);
+	}
+	if(x2 > x1 && x3 > x1){
+		return (x1 - x2 + x3);
+	}
+	if(x2 > x1 && x3 < x1){
+		return (x3 - x2 + x1);
+	}
 	
-	
+
 }
 
 function Y4(x1, y1, x2, y2, x3, y3){
@@ -36,6 +57,12 @@ function Y4(x1, y1, x2, y2, x3, y3){
 	}
 	if(X4(x1, y1, x2, y2, x3, y3) == "Ошибка"){
 		return "Ошибка";
+	}
+	if(y1 == y3 && y2 > y1){
+		return (y1 - y2);
+	}
+	if(y1 == y3 && y2 < y1){
+		return (y1 + y2);
 	}
 	if(y2 == y3){
 		return y1;
@@ -46,11 +73,35 @@ function Y4(x1, y1, x2, y2, x3, y3){
 	if(y1 == y3){
 		return y2;
 	}
-	if(y1 > y3){
-		return (y1 + y3 - y2);
+	if(y3 < y2 + y1 && x1 < x2){
+		return (y3 - y2 + y1);
 	}
-	if(y3 > y1){
+	if(y3 > y2 + y1 && x1 < x2){
 		return (y3 + y2 - y1);
+	}
+	if(y3 < y2 + y1 && x1 > x2){
+		return (y2 + y3 - y1);
+	}
+	if(y3 > y2 + y1 && x1 > x2){
+		return (y3 + y1 - y2);
+	}
+	if(y1 > y2 + y3){
+		return (y3 - y2 + y1);
+	}
+	if(y1 < y2 + y3){
+		return (y3 + y2 - y1);
+	}
+	if(y2 < y1 && y3 > y1){
+		return (y1 + y2 - y3);
+	}
+	if(y2 < y1 && y3 < y1){
+		return (y3 + y2 - y1);
+	}
+	if(y2 > y1 && y3 > y1){
+		return (y2 + y3 - y1);
+	}
+	if(y2 > y1 && y3 < y1){
+		return (y3 - y2 + y1);
 	}
 }
 
