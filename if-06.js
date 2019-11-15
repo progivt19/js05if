947c4de3a735840562b65622c27e80cb4646768f
Sivfrye <54879796+Sivfrye@users.noreply.github.com>
2019-11-15 14:33:00 +0900

    исправила

--- if-06.js
+++ if-06.js
@@ -2,15 +2,21 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function fmax(a,b,c,d){
-var min1=0; 
-var min2=0;
+function f(k, b, R){
+	var D = k * k * R * R - b * b + R * R;
 
-if (a<b) {min1=a} else {min1=b};
-if (c<d) {min2=c} else	{min2=d};
-if (min1<min2) 
-	return min1
- else min2;
+	if (D < 0) {
+		return 0;
+	}
+	
+	if (D > 0) {
+		return 2;
+	}
+
+	if (D === 0) {
+		return 1;
+	}
 }
 
-module.exports = fmax;
+
+module.exports = f
