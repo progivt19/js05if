52d40c16b011f7bd5593c3cde87a4202b70447d7
Prokop1y <54879791+pstn72@users.noreply.github.com>
2019-11-15 14:41:06 +0900

    задача №4

--- if-x4.js
+++ if-x4.js
@@ -2,9 +2,32 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
+function f(a, b, c, d){
+	a = Math.abs(a);
+	b = Math.abs(b);
+	c = Math.abs(c);
+	d = Math.abs(d);
+	var min1;
+	var max2;
 
-
+	if(a < b || a == b){
+		min1 = a;
+	}
+	if(a > b){
+		min1 = b;
+	}
+	if(c > d || c == d){
+		max2 = c;
+	}
+	if(c < d){
+		max2 = d;
+	}
+	if(min1 > max2 || min1 == max2){
+		return min1;
+	}
+	if(min1 < max2){
+		return max2;
+	}
 }
 
-module.exports = ...;
+module.exports = f;
