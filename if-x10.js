5a6b6e1697918dd84c3344ff91aed9f327ac0202
keltoblast <55118185+keltoblast@users.noreply.github.com>
2019-11-17 22:13:56 +0900

    Add files via upload

---
+++ if-x10.js
@@ -0,0 +1,50 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function MonthName(n){
+
+	switch(n) {
+	case 1:
+		name='январь'
+		break;
+	case 2:
+		name='январь'
+		break;
+	case 3:
+		name='январь'
+		break;
+	case 4:
+		name='январь'
+		break;
+	case 5:
+		name='январь'
+		break;
+	case 6:
+		name='январь'
+		break;
+	case 7:
+		name='январь'
+		break;
+	case 8:
+		name='январь'
+		break;
+	case 9:
+		name='январь'
+		break;
+	case 10:
+		name='январь'
+		break;
+	case 11:
+		name='январь'
+		break;
+	case 12:
+		name='январь'
+		break;
+		return'ошибка!'
+	}
+
+return name;
+	}
+
+module.exports = MonthName;
