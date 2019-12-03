91af634e9d3584c4829cf6ff6094a401eb448bcb
Tim Ivanov <tim.ivanov.st@gmail.com>
2019-12-03 20:34:07 +0900

    done

---
+++ if-12.js
@@ -0,0 +1,27 @@
+function digitName(n){
+	switch(true){
+		case n == 0:
+			return "ZERO";
+		case n == 1:
+			return "ONE";
+		case n == 2:
+			return "TWO";
+		case n == 3:
+			return "THREE";
+		case n == 4:
+			return "FOUR";
+		case n == 5:
+			return "FIVE";
+		case n == 6:
+			return "SIX";
+		case n == 7:
+			return "SEVEN";
+		case n == 8:
+			return "EIGHT";
+		case n == 9:
+			return "NINE";
+		case n > 9 || n < 0:
+			return "ERROR";
+	}
+}
+module.exports = digitName;
