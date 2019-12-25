6c83eac5d6deb4deb3a3a6d42f898387ce5e2457
Adamov Ayaal <aiaal.adamov@gmail.com>
2019-12-25 18:08:26 +0900

    New tests

--- test/if-13_test.js
+++ test/if-13_test.js
@@ -10,5 +10,8 @@ QUnit.test("ЗАДАЧА 13. S(x1,y1,x2,y2,x3,y3)=точки это вершин
 	assert.deepEqual(S(0,0,3,0,0,3), "true", "Не выполнено S(0,0,3,0,0,3) === true");
 	assert.deepEqual(S(1,4,1,1,5,1), "false", "Не выполнено S(1,4,1,1,5,1) === false");
 	assert.deepEqual(S(-2,2,-2,-3,1,-2), "false", "Не выполнено S(-2,2,-2,-3,1,-2) === false");
+	//Дальше новые тесты .
+	assert.deepEqual(S(1,3,4,4,5,1), "true", "Не выполнено S(1,3,4,4,5,1) === true");
+	assert.deepEqual(S(2,1,4,0,5,2), "true", "Не выполнено S(2,1,4,0,5,2) === true");
   }
 );
