f9fe14b67066bd9f18c9a149ea20d46e294fed6c
ruslanrb <54973228+ruslanrb@users.noreply.github.com>
2019-12-27 13:46:26 +0900

    Create if-05.js

---
+++ test/if-05.js
@@ -0,0 +1,10 @@
+const h = require('../if-05.js');
+
+QUnit.test("ЗАДАНИЕ 05. h(a, b, c)", function(assert){
+	assert.deepEqual(h(5, 4, 3),2, "Не выполнено h(a, b, c) === 2")
+	assert.deepEqual(h(15, 8, 7),-1, "Не выполнено h(a, b, c) === -1")
+	assert.deepEqual(h(5, 3, 3),1, "Не выполнено h(a, b, c) === 1")
+	assert.deepEqual(h(1, 1, 3),-1, "Не выполнено h(a, b, c) === -1")
+	assert.deepEqual(h(7, 9, 4),3, "Не выполнено h(a, b, c) === 3")
+	assert.deepEqual(h(40, 34, 22),18, "Не выполнено h(a, b, c) === 18")
+});
