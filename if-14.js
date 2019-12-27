cf3061c5ef813e54cacd822a0936eb4446c6a96a
zutorexdip <fjuluur@mail.ru>
2019-12-28 00:09:21 +0900

    sdelal testi

--- if-14.js
+++ if-14.js
@@ -1,10 +1,37 @@
-//TODO: Впишите имя функции из задания вместо ...
-//TODO: Добавьте правильные параметры
-//TODO: Напишите функцию
-
-function ...(){
-
-
+var a1=-1000; var b1=-2000; var c1=-3000;
+function X4(x1,y1,x2,y2,x3,y3){
+	if ( max_length(x1,y1,x2,y2,x3,y3)===a1)
+		return x1+x2-x3;
+	if ( max_length(x1,y1,x2,y2,x3,y3)===b1)
+		return x1+x3-x2;
+	if ( max_length(x1,y1,x2,y2,x3,y3)===c1)
+		return x2+x3-x1;
 }
 
-module.exports = ...;
+function Y4(x1,y1,x2,y2,x3,y3){
+	if ( max_length(x1,y1,x2,y2,x3,y3)===a1)
+		return y1+y2-y3;
+	if ( max_length(x1,y1,x2,y2,x3,y3)===b1)
+		return y1+y3-y2;
+	if ( max_length(x1,y1,x2,y2,x3,y3)===c1)
+		return y2+y3-y1;
+}
+
+function max_length(x1,y1,x2,y2,x3,y3) {
+	var a = Match.sqrt ((x1-x2)*(x1-x2)*(y1-y2)*(y1-y2));
+	var b = Match.sqrt ((x1-x3)*(x1-x3)*(y1-y3)*(y1-y3));
+	var c = Match.sqrt ((x3-x2)*(x3-x2)*(y3-y2)*(y3-y2));
+	
+	if (a>=b && a>=c)
+	return a1;
+	if (b>=a && b>=c)
+	return b1;
+	if (c>=a && c>=b)
+	return c1;
+}
+
+function XY(x1,y1,x2,y2,x3,y3) {
+	return "x4="+X4(x1,y1,x2,y2,x3,y3)+"y4="+Y4(x1,y1,x2,y2,x3,y3);
+}
+
+module.exports = XY;
---
+++ test/if-10_test.js
@@ -0,0 +1,11 @@
+  const MonthName = require('../if-10.js');
+
+QUnit.test("ЗАДАЧА 10. MonthName(n) возварщает название месяца с номером n", function(assert){
+	assert.deepEqual(MonthName(1), "январь", "Не выполнено MonthName(1) == январь")
+	assert.deepEqual(MonthName(2), "февраль", "Не выполнено MonthName(2) == февраль")
+	assert.deepEqual(MonthName(5), "май", "Не выполнено MonthName(5) == май")
+	assert.deepEqual(MonthName(3), "март", "Не выполнено MonthName(3) == март")
+	assert.deepEqual(MonthName(12), "декабрь", "Не выполнено MonthName(12) == декабрь")
+	assert.deepEqual(MonthName(10), "октябрь", "Не выполнено MonthName(10) == октябрь")
+	assert.deepEqual(MonthName(20), "", "Не выполнено 20");
+});
